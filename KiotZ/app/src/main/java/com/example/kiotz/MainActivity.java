7baa695c25519dce964ab30e4ee651aff1a062e4
Zinh <khongphaiemailcuat@nou.com>
2024-10-28 22:07:50 +0700

    Done form1.xml, designed a custom TextInput, you guys can use edittext_material_ui.xml as a template!

--- KiotZ/app/src/main/java/com/example/kiotz/MainActivity.java
+++ KiotZ/app/src/main/java/com/example/kiotz/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.edittext_material_ui);
 
         List<Product> products = Arrays.asList(
                 new Product(1, "Laptop", "Electronics", 999.99, "Piece", "/path/to/qr1"),
