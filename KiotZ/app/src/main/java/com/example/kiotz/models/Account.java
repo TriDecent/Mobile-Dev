3edf885f207a2c6f192335445c882ed5324a8f2a
TriDecent <116976929+TriDecent@users.noreply.github.com>
2024-11-09 00:42:29 +0700

    streamline to include only Username and Password, remove IIdentifiable implementation

--- KiotZ/app/src/main/java/com/example/kiotz/models/Account.java
+++ KiotZ/app/src/main/java/com/example/kiotz/models/Account.java
@@ -1,14 +1,4 @@
 package com.example.kiotz.models;
 
-public record Account(String UserName, String Password,
-                      String EmployeeID) implements IIdentifiable {
-    @Override
-    public String ID() {
-        return EmployeeID;
-    }
-
-    @Override
-    public IIdentifiable withId(String id) {
-        return new Account(UserName, Password, id);
-    }
+public record Account(String Email, String Password) {
 }
