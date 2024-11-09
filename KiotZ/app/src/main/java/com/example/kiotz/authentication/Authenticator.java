6ca5bce9cc6c25a587324cbe836c273dd725fe65
TriDecent <116976929+TriDecent@users.noreply.github.com>
2024-11-09 16:56:17 +0700

    implement method for redirecting to login screen upon user sign out

--- KiotZ/app/src/main/java/com/example/kiotz/authentication/Authenticator.java
+++ KiotZ/app/src/main/java/com/example/kiotz/authentication/Authenticator.java
@@ -1,24 +1,40 @@
 package com.example.kiotz.authentication;
 
+import android.content.Context;
+import android.content.Intent;
+
 import com.example.kiotz.models.Account;
+import com.example.kiotz.views.general.activities.LoginActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Authenticator {
+    private static Authenticator instance;
     private final FirebaseAuth mAuth;
 
-    public Authenticator() {
+    private Authenticator() {
         this.mAuth = FirebaseAuth.getInstance();
     }
 
+    public static synchronized Authenticator getInstance() {
+        if (instance == null) {
+            instance = new Authenticator();
+        }
+        return instance;
+    }
+
     public void signIn(Account account, OnCompleteListener<AuthResult> listener) {
-        mAuth.signInWithEmailAndPassword(account.Email(), account.Password()).addOnCompleteListener(listener);
+        mAuth
+                .signInWithEmailAndPassword(account.Email(), account.Password())
+                .addOnCompleteListener(listener);
     }
 
     public void register(Account account, OnCompleteListener<AuthResult> listener) {
-        mAuth.createUserWithEmailAndPassword(account.Email(), account.Password()).addOnCompleteListener(listener);
+        mAuth
+                .createUserWithEmailAndPassword(account.Email(), account.Password())
+                .addOnCompleteListener(listener);
     }
 
     public String getCurrentUserId() {
@@ -26,11 +42,18 @@ public class Authenticator {
         return currentUser != null ? currentUser.getUid() : null;
     }
 
-    public void signOut() {
+    public void signOut(Context context) {
         mAuth.signOut();
+        redirectToLogin(context);
     }
 
     public FirebaseUser getCurrentUser() {
         return mAuth.getCurrentUser();
+    }
+
+    private void redirectToLogin(Context context) {
+        Intent intent = new Intent(context, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
     }
 }
