6ac7b27273f080ece4658232e74523290e8b58d4
TriDecent <116976929+TriDecent@users.noreply.github.com>
2024-12-16 03:27:41 +0700

    refactor EmployeeDetailsDialog to improve variable scope and update display logic for employee details

--- KiotZ/app/src/main/java/com/example/kiotz/views/dialogs/EmployeeDetailsDialog.java
+++ KiotZ/app/src/main/java/com/example/kiotz/views/dialogs/EmployeeDetailsDialog.java
@@ -16,7 +16,9 @@ import java.util.Objects;
 
 public class EmployeeDetailsDialog {
     private final Context context;
-    private final Employee employee;
+    private Employee employee;
+
+    private TextView tvDialogId, tvDialogName, tvDialogEmail, tvDialogDate, tvGender, tvDialogPosition;
 
     public EmployeeDetailsDialog(Context context, Employee employee) {
         this.context = context;
@@ -29,33 +31,35 @@ public class EmployeeDetailsDialog {
         View dialogView = inflater.inflate(R.layout.employee_view_dialog, null);
         builder.setView(dialogView);
 
-        TextView tvDialogId = dialogView.findViewById(R.id.tv_dialog_id);
-        TextView tvDialogName = dialogView.findViewById(R.id.tv_dialog_name);
-        TextView tvDialogEmail = dialogView.findViewById(R.id.tv_dialog_email);
-        TextView tvDialogDate = dialogView.findViewById(R.id.tv_dialog_date);
-        TextView tvGender = dialogView.findViewById(R.id.tv_dialog_gender);
-        TextView tvDialogStatus = dialogView.findViewById(R.id.tv_dialog_position);
+        tvDialogId = dialogView.findViewById(R.id.tv_dialog_id);
+        tvDialogName = dialogView.findViewById(R.id.tv_dialog_name);
+        tvDialogEmail = dialogView.findViewById(R.id.tv_dialog_email);
+        tvDialogDate = dialogView.findViewById(R.id.tv_dialog_date);
+        tvGender = dialogView.findViewById(R.id.tv_dialog_gender);
+        tvDialogPosition = dialogView.findViewById(R.id.tv_dialog_position);
+
         ImageView ivEdit = dialogView.findViewById(R.id.iv_edit);
 
-        // Set initial student details
-        updateStudentDetails(tvDialogId, tvDialogName, tvDialogEmail, tvDialogDate, tvGender, tvDialogStatus);
+        displayStudentDetails(employee);
 
-        //        // Open EditStudentDialog with a callback to refresh this dialog's UI
-        //        ivEdit.setOnClickListener(v ->
-        //                new EditStudentDialog(context, employee, viewModel, (newStudent) -> {
-        //                    // Re-fetch updated student data and refresh UI
-        //                    updateStudentDetails(tvDialogId, tvDialogName, tvDialogEmail, tvDialogDate, tvDialogStatus, newStudent);
-        //                }).show()
-        //        );
+        ivEdit.setOnClickListener(v -> {
+            var dialog = new EmployeeEditedDialog(context, employee);
+            dialog.show();
+
+            dialog.setOnSaveClickListener(editedEmployee -> {
+                if (editedEmployee == employee) return;
+
+                employee = editedEmployee;
+
+                displayStudentDetails(employee);
+            });
+        });
 
         builder.setPositiveButton("OK", (dialog, which) -> dialog.dismiss());
         builder.create().show();
     }
 
-    private void updateStudentDetails(
-            TextView tvId, TextView tvName, TextView tvEmail,
-            TextView tvDate, TextView tvGender, TextView tvPosition) {
-
+    private void displayStudentDetails(Employee employee) {
         String employeeId = employee.ID();
         String employeeName = Objects.equals(employee.Name(), "") ? "N/A" : employee.Name();
         String employeeEmail = employee.Email();
@@ -63,11 +67,11 @@ public class EmployeeDetailsDialog {
         String employeeGender = employee.Gender() == null ? "N/A" : employee.Gender().toString();
         String employeePosition = employee.IsAdmin() ? "Manager" : "Employee";
 
-        tvId.setText(String.format(Locale.getDefault(), employeeId));
-        tvName.setText(employeeName == null ? "" : String.format(Locale.getDefault(), employeeName));
-        tvEmail.setText(String.format(Locale.getDefault(), employeeEmail));
-        tvDate.setText(employeeDate == null ? "" : String.format(Locale.getDefault(), employeeDate));
+        tvDialogId.setText(String.format(Locale.getDefault(), employeeId));
+        tvDialogName.setText(employeeName == null ? "" : String.format(Locale.getDefault(), employeeName));
+        tvDialogEmail.setText(String.format(Locale.getDefault(), employeeEmail));
+        tvDialogDate.setText(employeeDate == null ? "" : String.format(Locale.getDefault(), employeeDate));
         tvGender.setText(String.format(Locale.getDefault(), employeeGender));
-        tvPosition.setText(String.format(Locale.getDefault(), employeePosition));
+        tvDialogPosition.setText(String.format(Locale.getDefault(), employeePosition));
     }
 }
