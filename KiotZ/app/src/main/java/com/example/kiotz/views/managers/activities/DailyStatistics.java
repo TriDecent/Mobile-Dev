13d1fc880dba212ebaa70da80757035682b09a0c
Zinh <khongphaiemailcuat@nou.com>
2024-12-20 17:30:08 +0700

    added generic_avatar.png and item_invoice.xml, init ...

--- KiotZ/app/src/main/java/com/example/kiotz/views/managers/activities/DailyStatistics.java
+++ KiotZ/app/src/main/java/com/example/kiotz/views/managers/activities/DailyStatistics.java
@@ -1,6 +1,7 @@
 package com.example.kiotz.views.managers.activities;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +10,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.kiotz.R;
+import com.example.kiotz.database.FireBaseService;
+import com.example.kiotz.database.dto.ReceiptSerializer;
+import com.example.kiotz.inventory.Inventory;
+import com.example.kiotz.models.Receipt;
+import com.example.kiotz.repositories.Repository;
+import com.example.kiotz.viewmodels.InventoryViewModel;
+import com.example.kiotz.viewmodels.InventoryViewModelFactory;
 
 public class DailyStatistics extends AppCompatActivity {
+    ImageView change_sort_order_bt;
+    InventoryViewModel<Receipt> receiptViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +36,11 @@ public class DailyStatistics extends AppCompatActivity {
 
     private  void initVariable()
     {
-
+        change_sort_order_bt = findViewById(R.id.iv_sort_by_employee_name);
     }
 
     private void setupViewModel() {
-//        var employeeInventory = new Inventory<>(new Repository<>(new FireBaseService<>(new EmployeeSerializer())));
-//        employeeViewModel = InventoryViewModelFactory.getInstance().getViewModel(employeeInventory, Employee.class);
-//        TODO: reflect data on UI
+        var employeeInventory = new Inventory<>(new Repository<>(new FireBaseService<>(new ReceiptSerializer())));
+        receiptViewModel = InventoryViewModelFactory.getInstance().getViewModel(employeeInventory, Receipt.class);
     }
 }
