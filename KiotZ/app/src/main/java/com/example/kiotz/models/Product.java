d140d76b0901ee509a2ae99d20636bdd0a4be0f1
Zinh <khongphaiemailcuat@nou.com>
2024-12-20 16:07:10 +0700

    edited activity_create_product.xml to remove cost of capital

--- KiotZ/app/src/main/java/com/example/kiotz/models/Product.java
+++ KiotZ/app/src/main/java/com/example/kiotz/models/Product.java
@@ -1,6 +1,6 @@
 package com.example.kiotz.models;
 
-public record Product(String ID, String Name, String Category, double Price, String Unit,
+public record Product(String ID, String Name, String Category, double Price, String Unit, Integer Quantity,
                       String QRCodePath) implements IIdentifiable {
     @Override
     public String ID() {
@@ -9,6 +9,6 @@ public record Product(String ID, String Name, String Category, double Price, Str
 
     @Override
     public IIdentifiable withId(String id) {
-        return new Product(id, Name, Category, Price, Unit, QRCodePath);
+        return new Product(id, Name, Category, Price, Unit, Quantity, QRCodePath);
     }
 }
--- KiotZ/app/src/main/java/com/example/kiotz/views/general/activities/CreateProductActivity.java
+++ KiotZ/app/src/main/java/com/example/kiotz/views/general/activities/CreateProductActivity.java
@@ -1,6 +1,8 @@
 package com.example.kiotz.views.general.activities;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +11,25 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.kiotz.R;
+import com.example.kiotz.database.FireBaseService;
+import com.example.kiotz.database.dto.ProductSerializer;
+import com.example.kiotz.inventory.Inventory;
+import com.example.kiotz.models.Product;
+import com.example.kiotz.repositories.Repository;
+import com.example.kiotz.viewmodels.InventoryViewModel;
+import com.example.kiotz.viewmodels.InventoryViewModelFactory;
 
 public class CreateProductActivity extends AppCompatActivity {
+    private InventoryViewModel<Product> productViewModel;
+    private EditText id_et;
+    private  EditText name_et;
+    private  EditText price_et;
+    private  EditText quantity_et;
+    private  EditText unit_et;
+    private  EditText category;
+    private Button upload_img_bt;
+    private Button create_more_bt;
+    private Button complete_bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +41,25 @@ public class CreateProductActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        setupViewModel();
+    }
+
+    private void setupViewModel() {
+        var employeeInventory = new Inventory<>(new Repository<>(new FireBaseService<>(new ProductSerializer())));
+        productViewModel = InventoryViewModelFactory.getInstance().getViewModel(employeeInventory, Product.class);
+    }
+
+    private  void initVariables()
+    {
+        id_et = findViewById(R.id.editTextID);
+        name_et = findViewById(R.id.editTextName);
+        price_et = findViewById(R.id.editTextSellingPrice);
+        quantity_et = findViewById(R.id.editTextQuantily);
+        unit_et = findViewById(R.id.editUnit);
+        category = findViewById(R.id.editTextCategory);
+        upload_img_bt = findViewById(R.id.buttonUploadImg);
+        create_more_bt = findViewById(R.id.buttonCreateMore);
+        complete_bt = findViewById(R.id.buttonComplete);
     }
 }
